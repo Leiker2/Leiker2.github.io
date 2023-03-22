059b7dbbb47f3c303ec704c6b52b7d0df0f77189
leikerdgs <leiker.dgs@gmail.com>
2023-03-22 15:51:15 +0100

    Estas actualizaciones son parte de un curso JS

---
+++ js/js-curso-basico/1-valores.js
@@ -0,0 +1,13 @@
+40 //Number
+
+'leiker gomez' //String
+
+true //Boolean
+false //Boolean
+
+null 
+undefined
+
+{1,2,3} //object
+{nombre: 'Leiker'} //object
+[] // object
---
+++ js/js-curso-basico/2-variables.js
@@ -0,0 +1,3 @@
+var nombre = 'Oscar';
+
+// declarar 
