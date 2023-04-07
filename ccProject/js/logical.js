f73a7f34cc0e554a4f02f58bf97127d1a10dd7a5
leikerdgs <leiker.dgs@gmail.com>
2023-04-07 14:03:04 +0200

    Este es una actualizacion de CC page

--- ccProject/js/logical.js
+++ ccProject/js/logical.js
@@ -1,44 +1,20 @@
 const colorHEX = document.querySelector('.colorHEX');
 const container = document.querySelector('.container');
 const button = document.querySelector('.button');
+const seccion = document.querySelector('section');
 
-let i = 0;
+function aleatorio (){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+} 
+
+let color;
 button.addEventListener('click', function(){
     console.log('Le estas dando click en este momento al button y se va a cambiar el color');
-    i == 7 ? i = 0 : '';
-    switch (true){
-        case i == 0 :
-            colorHEX.textContent= '#231E39';
-            container.setAttribute ('class', 'container');  
-            i++;      
-        case i == 1:
-            colorHEX.textContent= '#ef5777';
-            container.setAttribute ('class', 'pink');
-            i++;
-            break;
-        case i == 2:
-            colorHEX.textContent = '#575fcf';
-            container.setAttribute ('class', 'periwinkle');
-            i++;
-            break;
-        case i == 3:
-            colorHEX.textContent= '#4bcffa';
-            container.setAttribute('class', 'megaman');
-            i++;
-            break;
-        case i == 4:
-            colorHEX.textContent = '#0be881';
-            container.setAttribute('class', 'minty');
-            i++;
-            break;
-        case i == 5:
-            colorHEX.textContent = '#ffd32a';
-            container.setAttribute('class','vibrant');
-            i++;
-            break;
-        case i == 6 :
-            colorHEX.textContent= '#231E39';
-            container.setAttribute ('class', 'blue');  
-            i++;     
-    }
+    color = seccion.style.backgroundColor = aleatorio();
+    colorHEX.textContent = color;
 })
