d5e5a5631ae50de33e98860fdf9ce8702bf254c1
leikerdgs <leiker.dgs@gmail.com>
2023-04-18 23:19:47 +0200

    Este es una actualizacion del curso EMAScript y un nuevo projecto

---
+++ curso-ecmascript/src/es7/00-exponencial.js
@@ -0,0 +1,4 @@
+//Este es exponencial, 3 elevado a la 4
+
+const data = 3 ** 4;
+console.log(data);
---
+++ curso-ecmascript/src/es8/01-object-values.js
@@ -0,0 +1,6 @@
+//object values
+//Este nos imprime solo los valores, sin el objeto
+
+const countries = { MX : 'Mexico', VE: 'Venezuela', CO:'Colombia', CL:'Chile' }
+
+console.log(Object.values(countries));
