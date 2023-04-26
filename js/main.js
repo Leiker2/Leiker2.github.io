926a2eed31ed27786cb220933c546d6a369617e1
leikerdgs <leiker.dgs@gmail.com>
2023-04-26 23:51:54 +0200

    This is a crazy change!!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // const pElement = document.querySelector('.fade-in');
 // window.addEventListener('scroll', () => {
 //     console.log('Scroll detectado');
@@ -9,47 +10,27 @@
 //       pElement.classList.add('active');
 //     }
 //   });
-
 //Here, comming to generate all projects
+const API = '../data/products.json';
 
-const projectList = [];
-
-projectList.push({
-    name: 'Huddle',
-    description: 'Este es un projecto bastante sensillo, disenado especificamente por Platzi, y fue hecho para culminar un desaf&iacute;o',
-    img: 'img/huddle.svg',
-    link: 'huddleProject/huddle.html'
-});
-
-projectList.push({
-    name: 'Change Color',
-    description: 'Este es un pequeno desaf&iacute;o de unos cursos de JS, es un simple programa que nos da n&uacute;meros aleatoreos y los transforma a colores RGB',
-    img: 'img/CCProject.svg',
-    link: 'ccProject/index.html'
-});
-projectList.push({
-    name: 'Counter',
-    description: 'Este es un simple contador, es una pr&aacute;ctica en cursos de JS, una ayuda de manejo del DOM',
-    img: 'img/Counter.svg',
-    link: 'counterProject/index.html'
-});
-projectList.push({
-    name: 'Yard Sales',
-    description: 'Este es un projecto manejado en con juntos con el curso de HTML,CSS, JS y react de Platzi',
-    img: 'img/yardsale.svg',
-    link: 'yardProject/index.html'
-});
-projectList.push({
-    name: 'Our Employee',
-    description: 'Este es un proyecto tomado para la pr&aacute;ctica del lenguaje JS',
-    img: 'img/OurEmployee.svg',
-    link: 'OurReviewsProject/index.html'
-});
+function fetchData(urlApi){
+    return fetch(urlApi)
+}
+ let projectList = [];
+fetchData(`${API}`)
+    .then(response => response.json())
+    .then(products => {
+        projectList = products;
+        console.log(projectList);
+        for (const project of projectList) {
+            projectorOfProductList(project)
+        }
+        
+    })
+    .catch(err => console.log(err))
+    .finally(() => console.log("finally"));
 
 let i = 0;
-for (const project of projectList) {
-    projectorOfProductList(project)
-}
 
 function projectorOfProductList(project){
     const portfolio = document.querySelector('.portfolio');
