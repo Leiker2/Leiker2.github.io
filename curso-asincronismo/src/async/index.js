d083a368829d911b5bcc2579aae57e31b1f07ee2
leikerdgs <leiker.dgs@gmail.com>
2023-05-04 21:57:13 +0200

    Here, changes for asicronism course

--- curso-asincronismo/src/async/index.js
+++ curso-asincronismo/src/async/index.js
@@ -15,3 +15,71 @@ const anotherFn = async () => {
 console.log('Before');
 anotherFn();
 console.log('After');
+
+
+
+//Practice
+
+const fnAsyn = () => {
+    return new Promise ((resolve,reject) => {
+        (true)
+            ? setTimeout(() => resolve('Async!!'),3000)
+            : reject(new Error ('Error!'))
+    })
+}
+
+const anotherfnn = async () => {
+    const something = await fnAsyn();
+    console.log('Hello');
+}
+
+anotherfnn()
+    .then(console.log('Before!!'))
+    .then(message => console.log(message))
+    .then(console.log('After!!'))
+    .catch((err) => console.log(err))
+    .finally(console.log('finally'))
+
+    //Practice again
+
+        let i = 2;
+    const FnAsync = () => {
+        return new Promise ((resolve,reject) => {
+            (i === 0)
+                ?setTimeout(() => resolve('Funciones asincronasss!'),5000)
+                :reject(new Error ('errrr!'));
+        })
+    }
+
+    const anotherFnAsync = async () => {
+        const something = await FnAsync();
+        console.log('Han pasado al menos 5s para que este texto se muestre')
+    }
+
+    console.log('Before, Before')
+
+    anotherFnAsync()
+        .then(console.log('Hola mundo!'))
+        .then(message => console.log(message))
+        .then(console.log('Afterr!!'))
+        .catch(err => console.log(err))
+
+//Practice again
+
+const functionAsync = () => {
+    return new Promise ((resolve,reject) => {
+        (true)
+            ? setTimeout(() => resolve('Async!!'), 10000)
+            : reject(new Error ('Error!'));
+    })
+} 
+
+const aanotherFunction = async () => {
+    const something = await functionAsync();
+    console.log('hello world')
+}
+
+console.log('Before');
+aanotherFunction();
+console.log('After');
+ 
